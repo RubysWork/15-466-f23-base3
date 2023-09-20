e7a1126adabcdbd75147b6228b049cbf1e6f50b6
RubysWork <yuew6@anddrew.cmu.edu>
2023-09-20 14:22:18 -0400

    basic

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -13,8 +13,8 @@
 #include <random>
 
 GLuint boss_meshes_for_lit_color_texture_program = 0;
-Load<MeshBuffer> hexapod_meshes(LoadTagDefault, []() -> MeshBuffer const *
-								{
+Load<MeshBuffer> boss_meshes(LoadTagDefault, []() -> MeshBuffer const *
+							 {
 	MeshBuffer const *ret = new MeshBuffer(data_path("boss.pnct"));
 	boss_meshes_for_lit_color_texture_program = ret->make_vao_for_program(lit_color_texture_program->program);
 	return ret; });
@@ -22,45 +22,30 @@ Load<MeshBuffer> hexapod_meshes(LoadTagDefault, []() -> MeshBuffer const *
 Load<Scene> boss_scene(LoadTagDefault, []() -> Scene const *
 					   { return new Scene(data_path("boss.scene"), [&](Scene &scene, Scene::Transform *transform, std::string const &mesh_name)
 										  {
-												 Mesh const &mesh = hexapod_meshes->lookup(mesh_name);
+											  Mesh const &mesh = boss_meshes->lookup(mesh_name);
 
-												 scene.drawables.emplace_back(transform);
-												 Scene::Drawable &drawable = scene.drawables.back();
+											  scene.drawables.emplace_back(transform);
+											  Scene::Drawable &drawable = scene.drawables.back();
 
-												 drawable.pipeline = lit_color_texture_program_pipeline;
+											  drawable.pipeline = lit_color_texture_program_pipeline;
 
-												 drawable.pipeline.vao = boss_meshes_for_lit_color_texture_program;
-												 drawable.pipeline.type = mesh.type;
-												 drawable.pipeline.start = mesh.start;
-												 drawable.pipeline.count = mesh.count; }); });
+											  drawable.pipeline.vao = boss_meshes_for_lit_color_texture_program;
+											  drawable.pipeline.type = mesh.type;
+											  drawable.pipeline.start = mesh.start;
+											  drawable.pipeline.count = mesh.count; }); });
 
-// Load<Sound::Sample> dusty_floor_sample(LoadTagDefault, []() -> Sound::Sample const *
-// 									   { return new Sound::Sample(data_path("dusty-floor.opus")); });
+Load<Sound::Sample> dusty_floor_sample(LoadTagDefault, []() -> Sound::Sample const *
+									   { return new Sound::Sample(data_path("dusty-floor.opus")); });
 
 PlayMode::PlayMode() : scene(*boss_scene)
 {
-	/*
 	// get pointers to leg for convenience:
 	for (auto &transform : scene.transforms)
 	{
-		if (transform.name == "Hip.FL")
-			hip = &transform;
-		else if (transform.name == "UpperLeg.FL")
-			upper_leg = &transform;
-		else if (transform.name == "LowerLeg.FL")
-			lower_leg = &transform;
+		if (transform.name == "Player")
+			player = &transform;
 	}
-	if (hip == nullptr)
-		throw std::runtime_error("Hip not found.");
-	if (upper_leg == nullptr)
-		throw std::runtime_error("Upper leg not found.");
-	if (lower_leg == nullptr)
-		throw std::runtime_error("Lower leg not found.");
 
-	hip_base_rotation = hip->rotation;
-	upper_leg_base_rotation = upper_leg->rotation;
-	lower_leg_base_rotation = lower_leg->rotation;
-*/
 	// get pointer to camera for convenience:
 	if (scene.cameras.size() != 1)
 		throw std::runtime_error("Expecting scene to have exactly one camera, but it has " + std::to_string(scene.cameras.size()));
@@ -133,24 +118,23 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 			return true;
 		}
 	}
-	else if (evt.type == SDL_MOUSEBUTTONDOWN)
-	{
-		if (SDL_GetRelativeMouseMode() == SDL_FALSE)
-		{
+	/*
+	else if (evt.type == SDL_MOUSEBUTTONDOWN) {
+		if (SDL_GetRelativeMouseMode() == SDL_FALSE) {
 			SDL_SetRelativeMouseMode(SDL_TRUE);
 			return true;
 		}
-	}
-	/*
-	else if (evt.type == SDL_MOUSEMOTION)
-	{
-		if (SDL_GetRelativeMouseMode() == SDL_TRUE)
-		{
+	} else if (evt.type == SDL_MOUSEMOTION) {
+		if (SDL_GetRelativeMouseMode() == SDL_TRUE) {
 			glm::vec2 motion = glm::vec2(
 				evt.motion.xrel / float(window_size.y),
-				-evt.motion.yrel / float(window_size.y));
+				-evt.motion.yrel / float(window_size.y)
+			);
 			camera->transform->rotation = glm::normalize(
-				camera->transform->rotation * glm::angleAxis(-motion.x * camera->fovy, glm::vec3(0.0f, 1.0f, 0.0f)) * glm::angleAxis(motion.y * camera->fovy, glm::vec3(1.0f, 0.0f, 0.0f)));
+				camera->transform->rotation
+				* glm::angleAxis(-motion.x * camera->fovy, glm::vec3(0.0f, 1.0f, 0.0f))
+				* glm::angleAxis(motion.y * camera->fovy, glm::vec3(1.0f, 0.0f, 0.0f))
+			);
 			return true;
 		}
 	}
@@ -160,57 +144,6 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 
 void PlayMode::update(float elapsed)
 {
-	/*
-		// slowly rotates through [0,1):
-		wobble += elapsed / 10.0f;
-		wobble -= std::floor(wobble);
-
-		hip->rotation = hip_base_rotation * glm::angleAxis(
-												glm::radians(5.0f * std::sin(wobble * 2.0f * float(M_PI))),
-												glm::vec3(0.0f, 1.0f, 0.0f));
-		upper_leg->rotation = upper_leg_base_rotation * glm::angleAxis(
-															glm::radians(7.0f * std::sin(wobble * 2.0f * 2.0f * float(M_PI))),
-															glm::vec3(0.0f, 0.0f, 1.0f));
-		lower_leg->rotation = lower_leg_base_rotation * glm::angleAxis(
-															glm::radians(10.0f * std::sin(wobble * 3.0f * 2.0f * float(M_PI))),
-															glm::vec3(0.0f, 0.0f, 1.0f));
-
-		// move sound to follow leg tip position:
-		leg_tip_loop->set_position(get_leg_tip_position(), 1.0f / 60.0f);
-	*/
-	// move camera:
-	{
-
-		// combine inputs into a move:
-		constexpr float PlayerSpeed = 30.0f;
-		glm::vec2 move = glm::vec2(0.0f);
-		if (left.pressed && !right.pressed)
-			move.x = -1.0f;
-		if (!left.pressed && right.pressed)
-			move.x = 1.0f;
-		if (down.pressed && !up.pressed)
-			move.y = -1.0f;
-		if (!down.pressed && up.pressed)
-			move.y = 1.0f;
-
-		// make it so that moving diagonally doesn't go faster:
-		if (move != glm::vec2(0.0f))
-			move = glm::normalize(move) * PlayerSpeed * elapsed;
-
-		glm::mat4x3 frame = camera->transform->make_local_to_parent();
-		glm::vec3 frame_right = frame[0];
-		// glm::vec3 up = frame[1];
-		glm::vec3 frame_forward = -frame[2];
-
-		camera->transform->position += move.x * frame_right + move.y * frame_forward;
-	}
-
-	{ // update listener to camera position:
-		glm::mat4x3 frame = camera->transform->make_local_to_parent();
-		glm::vec3 frame_right = frame[0];
-		glm::vec3 frame_at = frame[3];
-		Sound::listener.set_position_right(frame_at, frame_right, 1.0f / 60.0f);
-	}
 
 	// reset button press counters:
 	left.downs = 0;
@@ -263,9 +196,3 @@ void PlayMode::draw(glm::uvec2 const &drawable_size)
 	}
 	GL_ERRORS();
 }
-
-// glm::vec3 PlayMode::get_leg_tip_position()
-// {
-// 	// the vertex position here was read from the model in blender:
-// 	return lower_leg->make_local_to_world() * glm::vec4(-1.26137f, -11.861f, 0.0f, 1.0f);
-// }
