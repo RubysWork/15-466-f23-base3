7f7be4651cf62393f8413b875d337090057ad3bb
RubysWork <yuew6@anddrew.cmu.edu>
2023-09-21 08:38:55 -0400

    random row

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -11,6 +11,7 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include <random>
+#include <string>
 
 GLuint boss_meshes_for_lit_color_texture_program = 0;
 Load<MeshBuffer> boss_meshes(LoadTagDefault, []() -> MeshBuffer const *
@@ -58,6 +59,7 @@ PlayMode::PlayMode() : scene(*boss_scene)
 				bullet.isSafe = false;
 			direction_positions[bullet_index] = transform.position;
 			bullets.bullets[bullet_index] = bullet;
+			bullets.random_pos = direction_positions;
 			bullet_index++;
 			if (bullet_index > 7)
 			{
@@ -78,6 +80,7 @@ PlayMode::PlayMode() : scene(*boss_scene)
 				bullet.isSafe = false;
 
 			bullets.bullets[bullet_index] = bullet;
+			bullets.random_pos = direction_positions;
 			bullet_index++;
 			if (bullet_index > 7)
 			{
@@ -98,6 +101,7 @@ PlayMode::PlayMode() : scene(*boss_scene)
 				bullet.isSafe = false;
 
 			bullets.bullets[bullet_index] = bullet;
+			bullets.random_pos = direction_positions;
 			bullet_index++;
 			if (bullet_index > 7)
 			{
@@ -117,6 +121,7 @@ PlayMode::PlayMode() : scene(*boss_scene)
 				bullet.isSafe = false;
 
 			bullets.bullets[bullet_index] = bullet;
+			bullets.random_pos = direction_positions;
 			bullet_index++;
 			if (bullet_index > 7)
 			{
@@ -136,11 +141,12 @@ PlayMode::PlayMode() : scene(*boss_scene)
 				bullet.isSafe = false;
 
 			bullets.bullets[bullet_index] = bullet;
-			// transform.position = glm::vec3(0, 0, 0);
+			bullets.random_pos = direction_positions;
+
 			bullet_index++;
 			if (bullet_index > 7)
 			{
-				// bullets_list.emplace_back(bullets);
+
 				bullets_list[4] = bullets;
 				bullet_index = 0;
 			}
@@ -169,7 +175,6 @@ PlayMode::~PlayMode()
 
 bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 {
-
 	if (evt.type == SDL_KEYDOWN)
 	{
 		if (evt.key.keysym.sym == SDLK_ESCAPE)
@@ -177,131 +182,111 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 			SDL_SetRelativeMouseMode(SDL_FALSE);
 			return true;
 		}
-		else if (evt.key.keysym.sym == SDLK_a)
+		else if (evt.key.keysym.sym == SDLK_LEFT)
 		{
 			left.downs += 1;
 			left.pressed = true;
 			return true;
 		}
-		else if (evt.key.keysym.sym == SDLK_d)
+		else if (evt.key.keysym.sym == SDLK_RIGHT)
 		{
 			right.downs += 1;
 			right.pressed = true;
 			return true;
 		}
-		else if (evt.key.keysym.sym == SDLK_w)
+		else if (evt.key.keysym.sym == SDLK_UP)
 		{
 			up.downs += 1;
 			up.pressed = true;
 			return true;
 		}
-		else if (evt.key.keysym.sym == SDLK_s)
+		else if (evt.key.keysym.sym == SDLK_DOWN)
 		{
 			down.downs += 1;
 			down.pressed = true;
 			return true;
 		}
+		else if (evt.key.keysym.sym == SDLK_LSHIFT)
+		{
+			shift.downs += 1;
+			shift.pressed = true;
+			return true;
+		}
+		else if (evt.key.keysym.sym == SDLK_SPACE)
+		{
+			space.downs += 1;
+			space.pressed = true;
+			return true;
+		}
 	}
 	else if (evt.type == SDL_KEYUP)
 	{
-		if (evt.key.keysym.sym == SDLK_a)
+		if (evt.key.keysym.sym == SDLK_LEFT)
 		{
 			left.pressed = false;
 			return true;
 		}
-		else if (evt.key.keysym.sym == SDLK_d)
+		else if (evt.key.keysym.sym == SDLK_RIGHT)
 		{
 			right.pressed = false;
 			return true;
 		}
-		else if (evt.key.keysym.sym == SDLK_w)
+		else if (evt.key.keysym.sym == SDLK_UP)
 		{
 			up.pressed = false;
 			return true;
 		}
-		else if (evt.key.keysym.sym == SDLK_s)
+		else if (evt.key.keysym.sym == SDLK_DOWN)
 		{
 			down.pressed = false;
 			return true;
 		}
-	}
-	/*
-	else if (evt.type == SDL_MOUSEBUTTONDOWN) {
-		if (SDL_GetRelativeMouseMode() == SDL_FALSE) {
-			SDL_SetRelativeMouseMode(SDL_TRUE);
+		else if (evt.key.keysym.sym == SDLK_LSHIFT)
+		{
+			shift.pressed = false;
 			return true;
 		}
-	} else if (evt.type == SDL_MOUSEMOTION) {
-		if (SDL_GetRelativeMouseMode() == SDL_TRUE) {
-			glm::vec2 motion = glm::vec2(
-				evt.motion.xrel / float(window_size.y),
-				-evt.motion.yrel / float(window_size.y)
-			);
-			camera->transform->rotation = glm::normalize(
-				camera->transform->rotation
-				* glm::angleAxis(-motion.x * camera->fovy, glm::vec3(0.0f, 1.0f, 0.0f))
-				* glm::angleAxis(motion.y * camera->fovy, glm::vec3(1.0f, 0.0f, 0.0f))
-			);
+		else if (evt.key.keysym.sym == SDLK_SPACE)
+		{
+			space.pressed = false;
 			return true;
 		}
 	}
-*/
+
 	return false;
 }
 
 void PlayMode::update(float elapsed)
 {
 
+	// set bullets position
 	int bulletsindex = 0;
-	// std::list<Bullets> old_bullet_list;
 	for (auto &bullets : bullets_list)
 	{
-		// if out of screen, stop move
-		if (bullets.bullets[4].transform->position.x > -40 && bullets.bullets[4].transform->position.y < 25 && bullets.bullets[4].transform->position.y > -25)
-		{
-			// for (auto &bullet_list : old_bullet_list)
-			// {
-			// 	for (auto &bullet : bullet_list.bullets)
-			// 	{
-			// 		bullet.transform->position = glm::vec3(-46, -30, 1);
-			// 	}
-			// }
-			bullets.current_time += bullet_speed * elapsed;
-			for (auto &bullet : bullets.bullets)
+		bullets.current_time += bullet_speed * elapsed;
+
+		for (auto &bullet : bullets.bullets)
+		{ // if out of screen, stop move
+			if (bullet.transform->position.x > -60 && bullet.transform->position.y < 35 && bullet.transform->position.y > -35)
 			{
 				if (bullet.transform != nullptr)
 				{
-					bullet.transform->position = current_Pos(original_Pos, direction_positions[bullet.index], bullets.current_time);
+					bullet.transform->position = current_Pos(original_Pos, bullets.random_pos[bullet.index], bullets.current_time);
 				}
 			}
 		}
-		else
-		{
-			// old_bullet_list.emplace_back(bullets);
-
-			//  for (auto bullet : bullets.bullets)
-			//  {
-			//      if (bullet.transform != nullptr)
-			//      {
-			//          scene.drawables.remove(bullet.transform);
-			//          Scene::Drawable &drawable = scene.drawables.back();
-			//      }
-			//  }
-		}
-		// std::cout << "bullets" << bulletsindex << ": curent time: " << bullets.current_time << " x:  " << bullets.bullets[0].transform->position.x << ",y: " << bullets.bullets[0].transform->position.y << std::endl;
 		bulletsindex++;
 	}
-	//	std::cout << "bullets1:" << (bullets_list.begin())->bullets[0].transform->position.x << ",y: " << (bullets_list.begin())->bullets[0].transform->position.y << std::endl;
-	// std::cout << "bullets2:" << (bullets_list.begin())->bullets[0].transform->position.x << ",y: " << bullets.bullets[index].transform->position.y << std::endl;
 
+	// respawn
 	timer++;
-	// each 2 seconds, generate first beat
 	if (timer > 140)
 	{
 
-		bullets_list.begin()->current_time = 0;
+		bullets_list[0].current_time = 0;
+		bullets_list[0].random_pos = random_positions(direction_positions, 0);
 		// reset first beat position
-		for (auto &bullet : bullets_list.begin()->bullets)
+		for (auto &bullet : bullets_list[0].bullets)
 		{
 			bullet.transform->position = current_Pos(original_Pos, direction_positions[bullet.index], bullets_list.begin()->current_time);
 		}
@@ -314,10 +299,11 @@ void PlayMode::update(float elapsed)
 		if (beat3[beat3_index])
 		{
 			bullets_list[1].current_time = 0;
+			bullets_list[1].random_pos = random_positions(direction_positions, 12);
 			// reset first beat position
 			for (auto &bullet : bullets_list[1].bullets)
 			{
-				bullet.transform->position = current_Pos(original_Pos, direction_positions[bullet.index], bullets_list[1].current_time);
+				bullet.transform->position = current_Pos(original_Pos, bullets_list[1].random_pos[bullet.index], bullets_list[1].current_time);
 			}
 		}
 		beat3_index++;
@@ -330,10 +316,11 @@ void PlayMode::update(float elapsed)
 		if (beat4[beat4_index])
 		{
 			bullets_list[2].current_time = 0;
+			bullets_list[2].random_pos = random_positions(direction_positions, 24);
 			// reset first beat position
 			for (auto &bullet : bullets_list[2].bullets)
 			{
-				bullet.transform->position = current_Pos(original_Pos, direction_positions[bullet.index], bullets_list[2].current_time);
+				bullet.transform->position = current_Pos(original_Pos, bullets_list[2].random_pos[bullet.index], bullets_list[2].current_time);
 			}
 		}
 		beat4_index++;
@@ -346,10 +333,11 @@ void PlayMode::update(float elapsed)
 		if (beat5[beat5_index])
 		{
 			bullets_list[3].current_time = 0;
+			bullets_list[3].random_pos = random_positions(direction_positions, -12);
 			// reset first beat position
 			for (auto &bullet : bullets_list[3].bullets)
 			{
-				bullet.transform->position = current_Pos(original_Pos, direction_positions[bullet.index], bullets_list[3].current_time);
+				bullet.transform->position = current_Pos(original_Pos, bullets_list[3].random_pos[bullet.index], bullets_list[3].current_time);
 			}
 		}
 		beat5_index++;
@@ -362,10 +350,11 @@ void PlayMode::update(float elapsed)
 		if (beat6[beat6_index])
 		{
 			bullets_list[4].current_time = 0;
+			bullets_list[4].random_pos = random_positions(direction_positions, -24);
 			// reset first beat position
 			for (auto &bullet : bullets_list[4].bullets)
 			{
-				bullet.transform->position = current_Pos(original_Pos, direction_positions[bullet.index], bullets_list[4].current_time);
+				bullet.transform->position = current_Pos(original_Pos, bullets_list[4].random_pos[bullet.index], bullets_list[4].current_time);
 			}
 		}
 		beat6_index++;
@@ -377,20 +366,44 @@ void PlayMode::update(float elapsed)
 		if (beat7[beat7_index])
 		{
 			bullets_list[4].current_time = 0;
+			bullets_list[4].random_pos = random_positions(direction_positions, -36);
 			// reset first beat position
 			for (auto &bullet : bullets_list[4].bullets)
 			{
-				bullet.transform->position = current_Pos(original_Pos, direction_positions[bullet.index], bullets_list[4].current_time);
+				bullet.transform->position = current_Pos(original_Pos, bullets_list[4].random_pos[bullet.index], bullets_list[4].current_time);
 			}
 			beat7_index++;
 		}
 		timer5 = 0;
 	}
 
-	// scene.drawables.emplace_back(bullets[0].transform);
-
+	// detect touch
+	for (auto &bullets : bullets_list)
+	{
+		for (auto &bullet : bullets.bullets)
+		{
+			ishurt = false;
+			if (player->position.x > bullet.transform->position.x - 1.5 && player->position.x < bullet.transform->position.x + 1.5 && player->position.y > bullet.transform->position.y - 1.5 && player->position.y < bullet.transform->position.y + 1.5 && player->position.z < 2)
+			{
+				bullet.transform->position.x = -100;
+				bullet.transform->position.y = -50;
+				if (!ishurt)
+				{
+					if (bullet.isSafe)
+						life++;
+					else
+						life--;
+					ishurt = true;
+				}
+			}
+		}
+	}
 	// player control
-	constexpr float PlayerSpeed = 30.0f;
+	float PlayerSpeed = 30.0f;
+	if (shift.pressed)
+		PlayerSpeed = 10.0f;
+	else
+		PlayerSpeed = 30.0f;
 	glm::vec2 move = glm::vec2(0.0f);
 	if (left.pressed && !right.pressed)
 		move.x = -1.0f;
@@ -408,8 +421,34 @@ void PlayMode::update(float elapsed)
 	glm::vec3 frame_right = glm::vec3(0, -1, 0);
 	glm::vec3 frame_forward = glm::vec3(1, 0, 0);
 
+	// jump
+	float jump = 0.0f;
+	if (!isjump && !isjumping && space.pressed)
+	{
+		isjump = true;
+		isjumping = true;
+	}
+
+	if (isjump)
+	{
+		jump = +1.0f;
+	}
+	if (player->position.z > 8)
+		isjump = false;
+
+	if (!isjump)
+	{
+		if (player->position.z > 1.9)
+			jump -= 0.3f;
+		else if (player->position.z < 1.8)
+			player->position.z = 1.9f;
+		else
+			isjumping = false;
+	}
+
 	if ((player->position + (move.x * frame_right + move.y * frame_forward)).x < 5 && (player->position + (move.x * frame_right + move.y * frame_forward)).x > -35 && (player->position + (move.x * frame_right + move.y * frame_forward)).y < 20 && (player->position + (move.x * frame_right + move.y * frame_forward)).y > -20)
-		player->position += move.x * frame_right + move.y * frame_forward;
+		player->position += move.x * frame_right + move.y * frame_forward + jump * glm::vec3(0, 0, 1);
+
 	// std::cout << "player x:" << player->position.x << " player.y: " << player->position.y << std::endl;
 
 	// reset button press counters:
@@ -451,15 +490,15 @@ void PlayMode::draw(glm::uvec2 const &drawable_size)
 			0.0f, 0.0f, 0.0f, 1.0f));
 
 		constexpr float H = 0.09f;
-		lines.draw_text("Mouse motion rotates camera; WASD moves; escape ungrabs mouse",
+		lines.draw_text("Move - W/A/S/D; Jump - Space; Move Slow - Shift",
 						glm::vec3(-aspect + 0.1f * H, -1.0 + 0.1f * H, 0.0),
 						glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
 						glm::u8vec4(0x00, 0x00, 0x00, 0x00));
-		float ofs = 2.0f / drawable_size.y;
-		lines.draw_text("Mouse motion rotates camera; WASD moves; escape ungrabs mouse",
-						glm::vec3(-aspect + 0.1f * H + ofs, -1.0 + +0.1f * H + ofs, 0.0),
+
+		lines.draw_text("Life: " + std::to_string(life),
+						glm::vec3(-aspect + 0.1f * H + 2300.0f / drawable_size.y, -1.0 + +0.1f * H, 0.0),
 						glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
-						glm::u8vec4(0xff, 0xff, 0xff, 0x00));
+						glm::u8vec4(0xff, 0x00, 0x00, 0x00));
 	}
 	GL_ERRORS();
 }
@@ -468,4 +507,16 @@ glm::vec3 PlayMode::current_Pos(glm::vec3 origin_Pos, glm::vec3 final_Pos, float
 	glm::vec3 dir = glm::normalize(final_Pos - origin_Pos);
 	glm::vec3 current_Pos = glm::vec3(0, 0, 1) + dir * (1 + time);
 	return current_Pos;
+}
+
+std::array<glm::vec3, 8> PlayMode::random_positions(std::array<glm::vec3, 8> dir_pos, float offset)
+{
+	std::array<glm::vec3, 8> ran_pos;
+	for (int i = 0; i < dir_pos.size(); i++)
+	{
+		ran_pos[i] = dir_pos[i];
+		ran_pos[i].y -= offset;
+	}
+
+	return ran_pos;
 }
--- PlayMode.hpp
+++ PlayMode.hpp
@@ -20,6 +20,7 @@ struct PlayMode : Mode
 	virtual void update(float elapsed) override;
 	virtual void draw(glm::uvec2 const &drawable_size) override;
 	glm::vec3 current_Pos(glm::vec3 origin_Pos, glm::vec3 final_Pos, float time);
+	std::array<glm::vec3, 8> random_positions(std::array<glm::vec3, 8> direction_positions, float offset);
 	//----- game state -----
 
 	// input tracking:
@@ -27,13 +28,23 @@ struct PlayMode : Mode
 	{
 		uint8_t downs = 0;
 		uint8_t pressed = 0;
-	} left, right, down, up;
+	} left, right, down, up, shift, space;
 
 	// local copy of the game scene (so code can change it during gameplay):
 	Scene scene;
 	Scene::Transform *player = nullptr;
 	Scene::Transform *enemy = nullptr;
 
+	// life
+	int life = 5;
+	bool ishurt = false;
+
+	// jump
+	bool isjump = false;
+	bool isjumping = false;
+	int jumptimer = 0;
+
+	// Bullet
 	typedef struct Bullet
 	{
 		int index = 0;
@@ -45,6 +56,7 @@ struct PlayMode : Mode
 	{
 		std::array<Bullet, 8> bullets;
 		float current_time = 0;
+		std::array<glm::vec3, 8> random_pos;
 
 	} Bullets;
 	// Bullets origin_bullets;
@@ -53,8 +65,8 @@ struct PlayMode : Mode
 	glm::vec3 original_Pos = glm::vec3(0, 0, 0);
 
 	float bullet_speed = 20;
-	float current_time = 0;
 
+	// music
 	int timer = 0;
 	int timer1 = 0;
 	int timer2 = 0;
